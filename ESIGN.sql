5c4ad04bc8ea3216f0365ad0c6abf6413a374ca8
davyemane <davyemane2@gmail.com>
2024-08-06 14:26:45 +0100

    modified:   ESIGN.sql 	modified:   composer.json

--- ESIGN.sql
+++ ESIGN.sql
@@ -94,7 +94,7 @@ CREATE TABLE `user` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_USERNAME` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,6 +103,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'davyemane','[\"ROLE_USER\"]','$2y$13$PNCY5hu3ZTCzjsGROLSOR./jdTy7uLm3OHEhB3hpRIbumyHYEjf8G');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -115,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-06 13:46:19
+-- Dump completed on 2024-08-06 14:26:34
--- src/Security/LoginAuthenticatiorAuthenticator.php
+++ src/Security/LoginAuthenticatiorAuthenticator.php
@@ -48,9 +48,8 @@ class LoginAuthenticatiorAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Rediriger vers la page d'accueil après une connexion réussie
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
